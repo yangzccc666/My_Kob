65af69aa4d8428ee1bb55bde3685c63a389fd8ea
yangzccc <yangzccc@gmail.com>
2023-07-17 23:00:21 +0800

    创建前端页面——navbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,34 +1,17 @@
 <template>
-<div>
-  <div>Bot昵称：{{ bot_name }}</div>
-  <div>Bot战力：{{ bot_rating }}</div>
-</div>
+<NavBar></NavBar>
 </template>
 
 <script>
-import $ from "jquery";
-import { ref } from "vue";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import NavBar from './components/NavBar.vue';
 
 export default {
-  name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-    bot_name,
-    bot_rating
+  components: {
+    NavBar
   }
-  }
+
 }
 </script>
 
@@ -36,6 +19,6 @@ export default {
 <style>
 body {
   background-image: url("@/assets/bk.png");
-  background-size: cover;
+  background-size: cover; 
 }
 </style>
---
+++ web/src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">King Of Bots</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">对战</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">对局列表</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">排行榜</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      yzc
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+              </ul>
+          </div>
+        </div>
+      </nav>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
