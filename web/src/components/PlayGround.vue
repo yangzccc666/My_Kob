cf8c919c16e4da8b4225df2735685a27fef6b6b0
yangzccc <yangzccc@gmail.com>
2023-07-20 21:33:43 +0800

    7.20 创建pk页面

---
+++ web/src/components/PlayGround.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="playground">
+        <GameMap>
+
+        </GameMap>
+    </div>
+</template>
+
+<script>
+import  GameMap  from '@/components/GameMap.vue'
+
+export default {
+    components : {
+        GameMap,
+
+    }
+}
+</script>
+
+<style scoped>
+div.playground {
+    width: 60vw;
+    height: 70vh;
+    margin:0 auto;
+    margin-top: 40px;
+}
+</style>
