bf70c63162909ec64d19f0bc8dc34eedd1868396
yangzccc <yangzccc@gmail.com>
2023-07-31 22:18:47 +0800

    731 第四章mysql结束

--- web/src/store/user.js
+++ web/src/store/user.js
@@ -7,6 +7,7 @@ export default {
         photo: "",
         token: "",
         is_login: false,
+        pulling_info: true,
     },
     getters: {
     },
@@ -26,7 +27,11 @@ export default {
             state.photo = "";
             state.token = "";
             state.is_login = false;
+        },
+        updatePullingInfo(state, pulling_info) {
+            state.pulling_info = pulling_info;
         }
+
 
     },
     actions: {
@@ -40,6 +45,7 @@ export default {
                 },
                 success(resp) {
                     if (resp.error_message === "success") {
+                        localStorage.setItem("jwt_token", resp.token);
                         context.commit("updateToken", resp.token);
                         data.success(resp);
                     } else {
@@ -75,6 +81,7 @@ export default {
             })
         },
         logout(context) {
+            localStorage.removeItem("jwt_token");
             context.commit("logout");
         }
     },
